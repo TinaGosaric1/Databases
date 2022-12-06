b058e85a4b211e815069ad2a18aaa41c1f65c6ad
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-06 10:19:36 +0100

    More queries

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -22,6 +22,12 @@ public class Main {
         for (Artist artist : artists) {
             System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
         }
+
+        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Iron Maiden", Datasource.ORDER_BY_ASC);
+
+        for (String album : albumsForArtist) {
+            System.out.println(album);
+        }
         datasource.close();
     }
 }
