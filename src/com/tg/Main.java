9e6619c8c85a43ed1dfa441665558c6e5866abfb
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-06 09:25:31 +0100

    DB connection example

---
+++ src/com/tg/Main.java
@@ -0,0 +1,27 @@
+package com.tg;
+
+import com.tg.model.Artist;
+import com.tg.model.Datasource;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if (!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_ASC);
+        if (artists == null) {
+            System.out.println("No artists!");
+            return;
+        }
+
+        for (Artist artist : artists) {
+            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
+        }
+        datasource.close();
+    }
+}
