8999667f0405ba0b295a3949f27e483f84d07d11
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-07 13:12:16 +0100

    Actions to prevent an injection attack

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -15,46 +15,7 @@ public class Main {
             return;
         }
 
-        List<Artist> artists = datasource.queryArtists(Datasource.ORDER_BY_ASC);
-        if (artists == null) {
-            System.out.println("No artists!");
-            return;
-        }
-
-        for (Artist artist : artists) {
-            System.out.println("ID = " + artist.getId() + ", Name = " + artist.getName());
-        }
-
-        List<String> albumsForArtist = datasource.queryAlbumsForArtist("Pink Floyd", Datasource.ORDER_BY_ASC);
-
-        for (String album : albumsForArtist) {
-            System.out.println(album);
-        }
-
-        List<SongArtist> songArtists = datasource.queryArtistsForSong("Heartless", Datasource.ORDER_BY_ASC);
-        if (songArtists == null) {
-            System.out.println("Couldn't find the artist for the song");
-            return;
-        }
-
-        for (SongArtist artist : songArtists) {
-            System.out.println("Artist name = " + artist.getArtistName() +
-                    " Album name = " + artist.getAlbumName() +
-                    " Track = " + artist.getTrack());
-        }
-
-        datasource.querySongsMetadata();
-
-        int count = datasource.getCount(Datasource.TABLE_SONGS);
-        System.out.println("Number of songs is: " + count);
-
-        datasource.createViewForSongArtists();
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a song title: ");
-        String title = scanner.nextLine();
-
-        songArtists = datasource.querySongInfoView(title);
+        List<SongArtist> songArtists = datasource.querySongInfoView("Go Your Own Way", "The Dance");
 
         if (songArtists.isEmpty()) {
             System.out.println("Couldn't find the artist for the song");
@@ -62,9 +23,10 @@ public class Main {
         }
 
         for (SongArtist artist : songArtists) {
-            System.out.println("FROM VIEW - Artist name = " + artist.getArtistName() +
-                    " Album name = " + artist.getAlbumName() + " Track number = " + artist.getTrack());
+            System.out.println("Artist name = " + artist.getArtistName() +
+                    ", Album name = " + artist.getAlbumName() + ", Track number = " + artist.getTrack());
         }
+
         datasource.close();
     }
 }
