3876d921ba44bf322f7bde35f51c5c196c368457
Tina Gosarič <tina.gosaric@gorenje.com>
2022-12-07 16:44:34 +0100

    Transactions

--- src/com/tg/Main.java
+++ src/com/tg/Main.java
@@ -1,11 +1,6 @@
 package com.tg;
 
-import com.tg.model.Artist;
 import com.tg.model.Datasource;
-import com.tg.model.SongArtist;
-
-import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,18 +10,7 @@ public class Main {
             return;
         }
 
-        List<SongArtist> songArtists = datasource.querySongInfoView("Go Your Own Way", "The Dance");
-
-        if (songArtists.isEmpty()) {
-            System.out.println("Couldn't find the artist for the song");
-            return;
-        }
-
-        for (SongArtist artist : songArtists) {
-            System.out.println("Artist name = " + artist.getArtistName() +
-                    ", Album name = " + artist.getAlbumName() + ", Track number = " + artist.getTrack());
-        }
-
+        datasource.insertSong("Bird Dog", "Everly Brothers", "All-Time Greatest Hits", 7);
         datasource.close();
     }
 }
